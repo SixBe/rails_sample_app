994c2b09e7be78c85a050cddec2dc6f48c979acb
Michael <michael@sixbe.com>
2012-10-09 22:49:22 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   # match '/', to: 'static_pages#home'
   #match '/',        to: 'static_pages#home'
 
@@ -8,6 +10,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,66 +2,31 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+	subject{ page }
 
-  describe "Home page" do
-
-    it "should have the h1 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1', text: 'Sample App')
-    end
-
-    it "should have the base title" do
-	  visit root_path
-	  page.should have_selector('title', 
-	  		text: "#{base_title}")
+	describe "Home page" do
+		before {visit root_path}
+		it {should have_selector('h1', text: 'Sample App')}
+		it {should have_selector('title', text: full_title(''))}
+		it {should_not have_selector('title', text: '| Home')}
 	end
 
-	it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', text: '| Home')
-    end
-
-  end
-
-  describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
-    end
-  it "should have the right title" do
-	  visit help_path
-	  page.should have_selector('title', 
-	  		:text => "#{base_title} | Help")
+	describe "Help page" do
+		before {visit help_path}
+		it {should have_selector('h1', text: 'Help')}
+		it {should have_selector('title', text: full_title('Help'))}
 	end
-end
 
-  describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-it "should have the right title" do
-	  visit about_path
-	  page.should have_selector('title', 
-	  		:text => "#{base_title} | About Us")
+	describe "About page" do
+		before {visit about_path}
+		it {should have_selector('h1', text: 'About Us')}
+		it {should have_selector('title', text: full_title('About Us'))}
 	end
-  end
 
-describe "Contact page" do
-
-    it "should have the content 'Contact Us'" do
-      visit contact_path
-      page.should have_selector('h1', :text => 'Contact Us')
-    end
-
-it "should have the right title" do
-	  visit contact_path
-	  page.should have_selector('title', 
-	  		:text => "#{base_title} | Contact Us")
+	describe "Contact page" do
+		before {visit contact_path}
+		it {should have_selector('h1', text: 'Contact Us')}
+		it {should have_selector('title', text: full_title('Contact Us'))}
 	end
-  end
 end
