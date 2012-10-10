6803b85ba91df7891dd0106e0ffac8e2325616f1
Michael <michael@sixbe.com>
2012-10-10 15:46:49 +0200

    Finished exercices Chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
 		format: { with: VALID_EMAIL_REGEX }, 
 		uniqueness: { case_sensitive: false }
 
-	before_save { |user| user.email = email.downcase }
+	before_save { self.email.downcase! }
 
 	# Validation of password
 	validates :password, presence: true, length: { minimum: 6 }
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -51,5 +51,5 @@ describe "Static pages" do
     	page.should have_selector 'title', text: full_title('Sign Up')
     	click_link "sample app"
     	page.should have_selector 'title', text: full_title('')
-  end
+  	end
 end
